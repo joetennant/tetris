6fe9c7c2c0cbf3c09a840b0fb5fe2b8e97a77047
Joe Tennant <joetennant@gmail.com>
2025-12-17 12:51:11 -0500

    fix build?

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/tetris/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
